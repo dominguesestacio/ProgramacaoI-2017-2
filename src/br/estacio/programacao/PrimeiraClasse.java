025b4ec933bbcd4df62a7d081f4c992d23d909dc
aluno <aluno@MSCGRESL04008.estacio.acad>
2017-08-24 20:56:05 -0300

    Exercicio corrigido dia 24

--- src/br/estacio/programacao/PrimeiraClasse.java
+++ src/br/estacio/programacao/PrimeiraClasse.java
@@ -15,37 +15,38 @@ public class PrimeiraClasse {
      * @param args the command line arguments este guit esta funcionando
      */
     public static void main(String[] args) {
-        
+
         //Variaveis do tipo inteiro
         short variavelTipoShort = 10;
         int variavelDoTipoInt = 10;
         long variavelDoTipoLong = 1000;
+        byte variavelDoTipobyte = 10;
+        short variavelDoTiposhort = 10;
         
-         //variavel do tipo flutuante / real
+
+        //variavel do tipo flutuante / real
         float variavelDoTipoFloat = 134.2345f;
         double variavelDoTipoDouble = 12312312.123123123d;
-        
-         //variavel tipo logico recebe os valores true ou false
-         boolean variavelLogica = true;
-          
-         //variavel do tipo caracter
-         char variavelDoTipoCaracter = 'a';
-         
-         //objeto do tipo palavra/texto
-         String palavra = "Sou uma String";
-         
-         System.out.println("Helo Word");
-         System.out.println("Esta Variavel é do tipo byte: " + variavelDoTipobyte);
-         System.out.println("Esta Variavel é do tipo short: " + variavelDoTiposhort);
-         System.out.println("Esta Variavel é do tipo int: " + variavelDoTipoint);      
-         System.out.println("Esta Variavel é do tipo long: " + variavelDoTipolong);
-         System.out.println("Esta Variavel é do tipo float: " + variavelDoTipofloat);
-         System.out.println("Esta Variavel é do tipo double: " + variavelDoTipodouble);
-         System.out.println("Esta Variavel é do tipo doubleD: " + variavelDoTipodoubleD);
-         System.out.println("Esta Variavel é do tipo char: " + variavelDoTipoCaracter);
-         System.out.println("Esta Variavel é do tipo boolean: " + variavelDoTipoboolean);
-         System.out.println("Esta Variavel é do tipo String: " + palavra);
-        
+        //variavel tipo logico recebe os valores true ou false
+        boolean variavelLogica = true;
+
+        //variavel do tipo caracter
+        char variavelDoTipoCaracter = 'a';
+
+        //objeto do tipo palavra/texto
+        String palavra = "Sou uma String";
+
+        System.out.println("Helo Word");
+        System.out.println("Esta Variavel é do tipo byte: " + variavelDoTipobyte);
+        System.out.println("Esta Variavel é do tipo short: " + variavelDoTiposhort);
+        System.out.println("Esta Variavel é do tipo int: " + variavelDoTipoInt);
+        System.out.println("Esta Variavel é do tipo long: " + variavelDoTipoLong);
+        System.out.println("Esta Variavel é do tipo float: " + variavelDoTipoFloat);
+        System.out.println("Esta Variavel é do tipo double: " + variavelDoTipoDouble);
+        System.out.println("Esta Variavel é do tipo char: " + variavelDoTipoCaracter);
+        System.out.println("Esta Variavel é do tipo boolean: " + variavelLogica);
+        System.out.println("Esta Variavel é do tipo String: " + palavra);
+
     }
-    
+
 }
