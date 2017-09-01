f63901b19f012d550463e63631c51d03c34f7f7f
aluno <aluno@MSCGRESL04001.estacio.acad>
2017-08-31 21:16:10 -0300

    Aula Dia 31

---
+++ src/Aula4/TesteMultiplicacaoIntFloat.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aula4;
+
+/**
+ *
+ * @author aluno
+ */
+public class TesteMultiplicacaoIntFloat {
+    public static void main(String[] args) {
+        int x = 2;
+        float y = 3.1f;
+
+        float z;
+
+        //Organizar código ALT+SHIFT+F
+        
+        z = x * y;
+        //sout CTRL+SPACE
+        System.out.println(z);
+
+    }
+
+    
+}
---
+++ src/Aula5/TesteConcatenacao.java
@@ -0,0 +1,31 @@
+package Aula5;
+
+/**
+ *
+ * @author aluno
+ */
+public class TesteConcatenacao {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        String a = "String";
+        //primeiro exemplo//------------------------------------
+        //int b = 3;
+        //int c = 7;
+        //System.out.println(a + (b + c));
+        //segundo exemplo//-------------------------------------
+        //int d = 2;
+        //System.out.println("" + d + 3);
+        
+       //terceiro exemplo//-------------------------------------
+       int b= 3;
+       int c = 7;
+       int d = 50;
+        
+       System.out.println( a+(b + c)+d );
+    }
+    
+}
