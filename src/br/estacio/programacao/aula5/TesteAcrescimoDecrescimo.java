231441e7548523bafc5a4a3b88f8d3194c4de64f
aluno <aluno@MSCGRESL04008.estacio.acad>
2017-09-06 22:53:37 -0300

    Aula dia 06/09 corrigido

--- src/br/estacio/programacao/aula5/TesteAcrescimoDecrescimo.java
+++ src/br/estacio/programacao/aula5/TesteAcrescimoDecrescimo.java
@@ -32,11 +32,38 @@ public class TesteAcrescimoDecrescimo {
       
        int a = 10;
          if (a++ == 11){
-           System.out.println("Sucesso");
+           System.out.println("Sucesso" + a);
        } else { 
-           System.out.println("Ferrou");
-       }
+           System.out.println("Ferrou" + a);
            
+       }
+        //exemplo1
+         int b = 20;
+         int y = (b+=3);
+        System.out.println(y);
+        //exemplo2
+         int c = 10;
+         int z = c++ + ++c;
+         System.out.println(z);
+         
+         //para proxima aula
+         // int a = 3;
+         // int b = 1;
+         // int c = b + 1;
+         // int d = c > ++b ? (a -3) : b;
+         // int e = 4;
+         
+         // if( d > a && b + 1 < a || b > 1)
+         //   c += 7;
+         // if( -7 >= -c)
+         //   a -= 4;
+         // else d *= 2;
+         
+         // e = ++d % 3;
+         // b = d++ % 3;
+
+         
+         
     }
     
 }
