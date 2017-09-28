59147c771dd647867f19eee89d919e08e9fa1b5f
aluno <aluno@MSCGRESL04014.estacio.acad>
2017-09-27 22:21:29 -0400

    Aula dia 27/09

---
+++ src/ListaDeExercicios/Exercicio5.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ListaDeExercicios;
+
+/**
+ *
+ * @author aluno
+ */
+public class Exercicio5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 1;   
+        int c = b + 1;
+        int d = c > ++b ? (a - 3) : b; //d=1
+        // 'd' igual a 'c'2 se 'c'for maior do que 'b'2 = falso ele nao executa
+        // a-3 so atribui o valor de 'b'1
+        int e = 4;
+        if( d > a && b + 1 < a || b > 1)c += 7;
+        // 'd'1 é maior que 'a'3 = falso e nao executa a segunda parte
+        // atribui e=4
+        if( -7 >= -c)
+            a -= 4;
+        // -7 é maior ou igual '-c'-2
+        else 
+            d*= 2;
+        
+        e = ++d % 3;
+        // 'e' é igual '++d'2
+        
+        b = d++ % 3;
+
+        
+        
+        
+        System.out.println("valor de a: "+a);
+        System.out.println("valor de b: "+b);
+        System.out.println("valor de c: "+c);
+        System.out.println("valor de d: "+d);
+        System.out.println("valor de e: "+e);
+        
+    }
+    
+}
---
+++ src/ListaDeExercicios/Exercicio6.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ListaDeExercicios;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author aluno
+ */
+public class Exercicio6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    String saida="";
+ String entrada=JOptionPane.showInputDialog("Entre com um numero");
+
+    int i = Integer.parseInt(entrada);
+
+    if(i % 2 == 0)
+    saida+="O numero é par";
+    else
+    saida+="O numero é impar";
+
+ JOptionPane.showMessageDialog(null,
+"Valor "+saida,"Testa numero ",JOptionPane.INFORMATION_MESSAGE);
+System.exit(0);
+
+    }
+}
