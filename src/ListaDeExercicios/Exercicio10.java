dd3b16ee1da4b9709a10aaed5026c8cba39920eb
dominguesestacio <andredomingues.556@gmail.com>
2017-10-05 21:10:24 -0400

    TRABALHO PRONTO

--- src/ListaDeExercicios/Exercicio10.java
+++ src/ListaDeExercicios/Exercicio10.java
@@ -19,7 +19,7 @@ public class Exercicio10 {
                 for (c - 1; c < -a; c + -b) {
                     System.out.println("" + a + b + c);
                 }
-            }
+            }   //RESPOSTA 221
         }
     }
 
